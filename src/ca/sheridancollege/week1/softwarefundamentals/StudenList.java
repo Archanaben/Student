8de4f754b5b0e1acc4946a6a9c79ad9aa3b67797
Archanaben <pate2475@sheridancollege.ca>
2020-05-30 13:43:02 -0400

    First commit

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudenList.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author harsh
+ */
+public class StudenList {
+    public static void main(String[] args) 
+    {
+        //1 .Create an array of Students
+        Student[] sList = new Student[5];
+        //2. Put names in the array
+        for(int i = 0; i<=sList.length-1;i++)
+        {
+            sList[i] = new Student();
+            sList[i].setName("Student " + (i+1) );
+        }
+        //3. Print the array
+        for(Student s:sList)
+            System.out.println("The Student name is: "+s.getName());
+    }
+}
